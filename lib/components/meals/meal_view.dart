44bc75bfe20de4344d4f6cc21764a4b299b595d3
Moritz Huber <2nd@moritzhuber.de>
2023-05-09 23:41:09 +0200

    Redesign (#17)
    * started material3
    
    * filter canteens
    
    * filter canteens and replace bloc with provider
    
    * monochrome icon
    
    * center canteen picker

--- lib/components/meals/meal_view.dart
+++ lib/components/meals/meal_view.dart
@@ -1,81 +1,66 @@
-import 'package:flutter/material.dart';
+import 'package:better_hm/extensions/extensions_context.dart';
 import 'package:better_hm/models/meal/day.dart';
 import 'package:better_hm/models/meal/dish.dart';
+import 'package:flutter/material.dart';
 
 class MealView extends StatelessWidget {
   const MealView({super.key, required this.day});
 
   final MealDay day;
 
-  Map<String, List<Dish>> groupDishes() {
-    final Map<String, List<Dish>> grouped = {};
-
-    for (var element in day.dishes) {
-      grouped[element.dishType] == null
-          ? grouped[element.dishType] = [element].toList()
-          : grouped[element.dishType]!.add(element);
-    }
-    return grouped;
-  }
-
   @override
   Widget build(BuildContext context) {
     return SelectionArea(
       child: ListView(
         shrinkWrap: true,
-        children: groupDishes()
-            .map((dishType, dishes) => MapEntry(
-                dishType,
-                DishSection(
-                  dishType: dishType,
-                  dishes: dishes,
-                )))
-            .values
+        children: day.dishes
+            .where(
+                (element) => !["Aktion", "Beilagen"].contains(element.dishType))
+            .map((dish) => DishCard(dish: dish))
             .toList(),
       ),
     );
   }
 }
 
-class DishSection extends StatelessWidget {
-  const DishSection({super.key, required this.dishType, required this.dishes});
-
-  final String dishType;
-  final List<Dish> dishes;
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(bottom: 8.0),
-              child: Text(
-                dishType,
-                style: TextStyle(color: Theme.of(context).colorScheme.primary),
-              ),
-            ),
-            ListView(
-              shrinkWrap: true,
-              children: dishes.map((dish) => DishRow(dish: dish)).toList(),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class DishRow extends StatelessWidget {
-  const DishRow({super.key, required this.dish});
+class DishCard extends StatelessWidget {
+  const DishCard({
+    Key? key,
+    required this.dish,
+  }) : super(key: key);
 
   final Dish dish;
 
   @override
   Widget build(BuildContext context) {
-    return Text(dish.name);
+    return Card(
+      child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.all(Radius.circular(100)),
+                    color: context.theme.colorScheme.primaryContainer),
+                padding:
+                    const EdgeInsets.symmetric(vertical: 2.0, horizontal: 8.0),
+                child: Text(dish.dishType),
+              ),
+              const SizedBox(height: 8),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Expanded(
+                    child: Text(
+                      dish.name,
+                      style: context.theme.textTheme.bodyLarge,
+                    ),
+                  )
+                ],
+              ),
+            ],
+          )),
+    );
   }
 }
