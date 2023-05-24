441e9295ac155bb701e674eb5892477324d3feb0
Moritz Huber <2nd@moritzhuber.de>
2023-05-24 16:42:50 +0200

    basic labels

--- lib/models/meal/dish.dart
+++ lib/models/meal/dish.dart
@@ -1,15 +1,19 @@
+import 'package:better_hm/models/meal/label.dart';
 import 'package:better_hm/models/meal/prices.dart';
 
 class Dish {
   final String name;
   final MealPrices prices;
   final String dishType;
+  final Labels labels;
 
-  Dish(this.name, this.prices, this.dishType);
+  Dish(this.name, this.prices, this.dishType, this.labels);
 
   static Dish fromJson(Map<String, dynamic> json) => Dish(
         json["name"],
         MealPrices.fromJson(json["prices"]),
         json["dish_type"],
+        Labels.fromStringList(
+            (json["labels"] as List).map((e) => e.toString()).toList()),
       );
 }
