f452cba661903da895ba452a8b115f596edf24b6
Moritz Huber <2nd@moritzhuber.de>
2023-05-28 17:31:11 +0200

    fix merge

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:better_hm/i18n/strings.g.dart';
 import 'package:dynamic_color/dynamic_color.dart';
 import 'package:flutter/material.dart';
 import 'package:better_hm/routes.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
--- lib/routes.dart
+++ lib/routes.dart
@@ -1,4 +1,4 @@
-import 'package:better_hm/extensions/extensions_context.dart';
+import 'package:better_hm/i18n/strings.g.dart';
 import 'package:better_hm/screens/dashboard_screen.dart';
 import 'package:better_hm/screens/meals_screen.dart';
 import 'package:flutter/material.dart';
@@ -9,7 +9,7 @@ final router = GoRouter(
     ShellRoute(
       builder: (BuildContext context, GoRouterState state, Widget child) {
         return Scaffold(
-          appBar: AppBar(title: Text(context.l10n.app_name)),
+          appBar: AppBar(title: Text(t.app_name)),
           body: child,
           bottomNavigationBar: NavigationBar(
             onDestinationSelected: (int index) {
@@ -20,11 +20,11 @@ final router = GoRouter(
             destinations: [
               NavigationDestination(
                 icon: const Icon(Icons.home_rounded),
-                label: context.l10n.dashboard,
+                label: t.navigation.dashboard,
               ),
               NavigationDestination(
                 icon: const Icon(Icons.restaurant_rounded),
-                label: context.l10n.meals,
+                label: t.navigation.mealplan,
               ),
             ],
           ),
