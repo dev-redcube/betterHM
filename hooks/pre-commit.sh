caa34f664322ff62c7f124f3293088001eef3b8b
Moritz Huber <2nd@moritzhuber.de>
2023-05-30 22:10:23 +0200

    add pre-commit hook and setup script

---
+++ hooks/pre-commit.sh
@@ -0,0 +1,13 @@
+flutter analyze .
+
+# Get all staged dart files
+staged=$(git -P diff --name-only --cached --diff-filter=AM | grep -E '\.dart$')
+
+# exit if nothing matches
+[[ -z "$staged" ]] && exit 0
+
+# shellcheck disable=SC2086
+dart format $staged
+
+# shellcheck disable=SC2086
+git add $staged
