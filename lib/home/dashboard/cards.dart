78a80b824d8852e792369cbdfceaa0f5b4bc4e52
Moritz Huber <2nd@moritzhuber.de>
2023-06-17 20:39:25 +0200

    cards auslagern

---
+++ lib/home/dashboard/cards.dart
@@ -0,0 +1,24 @@
+import 'package:better_hm/home/dashboard/dashboard_card.dart';
+import 'package:better_hm/home/dashboard/mvg/departure.dart';
+import 'package:better_hm/home/dashboard/mvg/next_departures.dart';
+import 'package:better_hm/home/dashboard/mvg/service/api_mvg.dart';
+import 'package:better_hm/home/dashboard/semester_status/api_semester_status.dart';
+import 'package:better_hm/home/dashboard/semester_status/models/semester_event.dart';
+import 'package:better_hm/home/dashboard/semester_status/semester_status.dart';
+import 'package:better_hm/i18n/strings.g.dart';
+
+final cards = <DashboardCard>{
+  DashboardCard<List<SemesterEvent>>(
+    title: t.dashboard.statusCard.title,
+    cardId: "SEMESTER_STATUS",
+    card: (events) => SemesterStatus(events: events),
+    future: () => ApiSemesterStatus().getEvents(),
+  ),
+  DashboardCard<List<Departure>>(
+    title: t.dashboard.mvg.title,
+    cardId: "NEXT_DEPARTURES",
+    card: (departures) => NextDepartures(departures: departures),
+    future: () =>
+        ApiMvg().getDepartures(stopId: stopIdLothstr, lineIds: lineIdsLothstr),
+  )
+};
--- lib/home/dashboard/dashboard_screen.dart
+++ lib/home/dashboard/dashboard_screen.dart
@@ -1,35 +1,14 @@
+import 'package:better_hm/home/dashboard/cards.dart';
 import 'package:better_hm/home/dashboard/dashboard_card.dart';
 import 'package:better_hm/home/dashboard/manage_cards.dart';
-import 'package:better_hm/home/dashboard/mvg/departure.dart';
-import 'package:better_hm/home/dashboard/mvg/next_departures.dart';
-import 'package:better_hm/home/dashboard/mvg/service/api_mvg.dart';
-import 'package:better_hm/home/dashboard/semester_status/api_semester_status.dart';
-import 'package:better_hm/home/dashboard/semester_status/models/semester_event.dart';
-import 'package:better_hm/home/dashboard/semester_status/semester_status.dart';
 import 'package:better_hm/i18n/strings.g.dart';
 import 'package:better_hm/shared/logger/logger.dart';
 import 'package:better_hm/shared/prefs.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
-final dashboardCards = <DashboardCard>{
-  DashboardCard<List<SemesterEvent>>(
-    title: t.dashboard.statusCard.title,
-    cardId: "SEMESTER_STATUS",
-    card: (events) => SemesterStatus(events: events),
-    future: () => ApiSemesterStatus().getEvents(),
-  ),
-  DashboardCard<List<Departure>>(
-    title: t.dashboard.mvg.title,
-    cardId: "NEXT_DEPARTURES",
-    card: (departures) => NextDepartures(departures: departures),
-    future: () =>
-        ApiMvg().getDepartures(stopId: stopIdLothstr, lineIds: lineIdsLothstr),
-  )
-};
-
 DashboardCard? getCardFromId(String cardId) =>
-    dashboardCards.where((element) => element.cardId == cardId).firstOrNull;
+    cards.where((element) => element.cardId == cardId).firstOrNull;
 
 class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
