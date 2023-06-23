25b6517c0104a3906427d9896d4c23544fc34ede
Moritz Huber <2nd@moritzhuber.de>
2023-06-23 19:46:57 +0200

    update find in setup

--- setup.sh
+++ setup.sh
@@ -8,6 +8,6 @@ flutter clean
 flutter pub get
 
 # run build runner, for translations, database, etc.
-find -name "*.g.dart" -type f -delete
-find -name "*.mocks.dart" -type f -delete
+find . -name "*.g.dart" -type f -delete
+find . -name "*.mocks.dart" -type f -delete
 dart run build_runner build --delete-conflicting-outputs
