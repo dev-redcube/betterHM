867be00817c7c03fa31e15976da7ceca469beeeb
Moritz Huber <2nd@moritzhuber.de>
2023-07-06 15:24:57 +0200

    fix: semester information api endpoint change

--- lib/home/dashboard/semester_status/api_semester_status.dart
+++ lib/home/dashboard/semester_status/api_semester_status.dart
@@ -7,7 +7,7 @@ import 'package:better_hm/shared/service/api_service.dart';
 
 class ApiSemesterStatus extends ApiService {
   static const url =
-      "https://raw.githubusercontent.com/adriangeorgimmel/hm-dates-api/gh-pages/thisSemester/all.json";
+      "https://api.betterhm.app/hm-dates-api/thisSemester/all.json";
 
   Future<List<SemesterEvent>> getEvents() async {
     Logger("semester status").info("Fetching Events from Server...");
