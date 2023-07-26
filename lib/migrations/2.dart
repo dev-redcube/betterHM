cdaeb3a23373a386584bd4f6d21fbbc8a2c0ea33
Moritz <2nd@moritzhuber.de>
2023-07-26 22:26:11 +0200

    migration

--- lib/migrations/2.dart
+++ lib/migrations/2.dart
@@ -1,9 +1,12 @@
 // ignore_for_file: file_names
 
+import 'package:better_hm/shared/prefs.dart';
+
 /// For version 1.1
 /// Migrate cards to the new format
 Future<void> migration2() async {
-  /// TODO:
-  /// Cards: leadTime => offset
-  ///
+  await Prefs.cards.waitUntilLoaded();
+  List<String> cardsConfig = Prefs.cards.value;
+  Prefs.cards.value =
+      cardsConfig.map((e) => e.replaceAll("leadTime", "offset")).toList();
 }
