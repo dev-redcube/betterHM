809527bebbdb3b8185aa03b18027be97efd06048
Moritz Huber <2nd@moritzhuber.de>
2023-11-14 11:56:07 +0000

    line colors

--- lib/home/dashboard/sections/mvg/line_icon.dart
+++ lib/home/dashboard/sections/mvg/line_icon.dart
@@ -1,23 +1,45 @@
 import 'package:better_hm/shared/extensions/extensions_context.dart';
 import 'package:flutter/material.dart';
 
+const busColor = Color(0xFF005262);
+const tramColor = Color(0xFFe30613);
+
+const genericBus = (Colors.white, BoxDecoration(color: busColor));
+
 final lineColors = <String, (Color, BoxDecoration)>{
-  "20": (Colors.white, const BoxDecoration(color: Color(0xFF56B8E2))),
-  "21": (Colors.white, const BoxDecoration(color: Color(0xFFBF9737))),
-  "19": (Colors.white, const BoxDecoration(color: Color(0xFFe30613))),
-  "27": (Colors.white, const BoxDecoration(color: Color(0xFFf7a600))),
-  "28": (
-    const Color(0xFFf7a600),
-    BoxDecoration(border: Border.all(color: const Color(0xFFf7a600), width: 2))
-  ),
+  // Lothstraße
+  "20": (Colors.white, const BoxDecoration(color: Color(0xFF16bae7))), // Tram
+  "21": (Colors.white, const BoxDecoration(color: Color(0xFFc79514))), // Tram
   "29": (
     const Color(0xFFD02E26),
     BoxDecoration(border: Border.all(color: const Color(0xFFD02E26), width: 2))
-  ),
-  "153": (Colors.white, const BoxDecoration(color: Color(0xFF205060))),
-  "160": (Colors.white, const BoxDecoration(color: Color(0xFF005262))),
-  "N20": (Colors.white, const BoxDecoration(color: Color(0xFF16BAE7))),
-  "N27": (Colors.white, const BoxDecoration(color: Color(0xFFf7a600))),
+  ), // Tram
+  "N20": (Colors.white, const BoxDecoration(color: Color(0xFF16bae7))), // Tram
+  "153": genericBus, // Bus
+  // Avenariusplatz
+  "160": genericBus, // Bus
+  // Planegger Straße
+  "56": (Colors.white, const BoxDecoration(color: Color(0xFFec6726))), // Bus
+  "57": (Colors.white, const BoxDecoration(color: Color(0xFFec6726))), // Bus
+  "259": genericBus, // Bus
+  "265": genericBus, // Bus
+  // Ottostraße
+  "27": (
+    Colors.white,
+    const BoxDecoration(color: Color(0xFFf7a600))
+  ), // Tram SEV
+  "727": genericBus, // Bus SEV
+  // Lenbachplatz
+  "19": (Colors.white, const BoxDecoration(color: tramColor)), // Tram
+  // 21 -> siehe Lothstraße
+  "N19": (Colors.white, const BoxDecoration(color: tramColor)), // Tram
+  "N40": genericBus, // Bus
+  "N41": genericBus, // Bus
+  "N45": genericBus, // Bus
+  "N27": (
+    const Color(0xFFf2ad13),
+    const BoxDecoration(color: Colors.black)
+  ), // Tram
 };
 
 class LineIcon extends StatelessWidget {
