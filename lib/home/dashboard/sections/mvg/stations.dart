b9f28d7234f58bd7a95757b9c7bcb30cda5d9aba
Moritz Huber <2nd@moritzhuber.de>
2023-11-24 19:04:19 +0100

    nearest station logger

--- lib/home/dashboard/sections/mvg/stations.dart
+++ lib/home/dashboard/sections/mvg/stations.dart
@@ -3,6 +3,7 @@ import 'package:better_hm/shared/models/location.dart';
 import 'package:better_hm/shared/service/location_service.dart';
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import 'package:logging/logging.dart';
 
 class Station {
   final String name;
@@ -25,11 +26,14 @@ abstract class StationService {
       stations.where((element) => element.id == id).firstOrNull;
 
   static Future<Station?> getNearestStation() async {
+    final logger = Logger("MvgSection");
+
     final locationService = getIt<LocationService>();
     Position? position;
     try {
       position = await locationService.determinePosition();
-    } catch (e) {
+    } catch (e, stacktrace) {
+      logger.warning("Failed to get location", e, stacktrace);
       return null;
     }
 
@@ -42,7 +46,20 @@ abstract class StationService {
         )
         .toList();
     nearest.sort((a, b) => a.$2.compareTo(b.$2));
-    return nearest.firstOrNull?.$1;
+
+    final nearestStation = nearest.firstOrNull?.$1;
+
+    if (nearestStation != null) {
+      logger.info(
+        "Nearest Station: ${nearestStation.name}. Location: lat. ${position.latitude} lon. ${position.longitude}. Accuracy: ${position.accuracy}",
+      );
+    } else {
+      logger.warning(
+        "Failed to get nearest Station. Location: lat. ${position.latitude} lon. ${position.longitude}. Accuracy: ${position.accuracy}",
+      );
+    }
+
+    return nearestStation;
   }
 }
 
