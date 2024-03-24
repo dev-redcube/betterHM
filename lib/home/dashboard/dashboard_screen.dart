5606042002fb4452375fb8f9f5e480e30a7e5b50
Moritz <2nd@moritzhuber.de>
2024-03-24 14:15:19 +0100

    restore dashboard cards

--- lib/home/dashboard/dashboard_screen.dart
+++ lib/home/dashboard/dashboard_screen.dart
@@ -1,3 +1,5 @@
+import 'package:better_hm/home/dashboard/sections/kino/kino_section.dart';
+import 'package:better_hm/home/dashboard/sections/mvg/mvg_section.dart';
 import 'package:better_hm/home/dashboard/student_information.dart';
 import 'package:better_hm/i18n/strings.g.dart';
 import 'package:better_hm/settings/settings_screen.dart';
@@ -27,8 +29,8 @@ class DashboardScreen extends StatelessWidget {
           children: const [
             TopInformation(),
             Divider(),
-            // MvgSection(),
-            // KinoSection(),
+            MvgSection(),
+            KinoSection(),
           ],
         ),
       ),
