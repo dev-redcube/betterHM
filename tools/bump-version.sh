634c477bf78f6c329e47b10720e7c338eaaa15ad
Moritz Huber <2nd@moritzhuber.de>
2024-03-26 22:15:00 +0100

    update release action

---
+++ tools/bump-version.sh
@@ -0,0 +1,33 @@
+BUMP="$1"
+
+CURRENT_BUILD_NUMBER=$(grep "^version: .*+[0-9]\+$" pubspec.yaml | cut -d "+" -f2)
+NEXT_BUILD_NUMBER=$CURRENT_BUILD_NUMBER
+
+set $((NEXT_BUILD_NUMBER++))
+
+CURRENT_VERSION=$(grep -o -P "(?<=version: )\d+\.\d+\.\d+" pubspec.yaml)
+MAJOR=$(echo "$CURRENT_VERSION" | cut -d '.' -f1)
+MINOR=$(echo "$CURRENT_VERSION" | cut -d '.' -f2)
+PATCH=$(echo "$CURRENT_VERSION" | cut -d '.' -f3)
+
+echo "$CURRENT_VERSION"
+
+if [[ "$BUMP" == "major" ]]; then
+  MAJOR=$((MAJOR + 1))
+  MINOR=0
+  PATCH=0
+elif [ "$BUMP" == "minor" ]; then
+  MINOR=$((MINOR + 1))
+  PATCH=0
+elif [ "$BUMP" == "patch" ]; then
+  PATCH=$((PATCH + 1))
+else
+    echo 'Expected <major|minor|patch> for the server argument'
+    exit 1;
+fi
+
+NEXT_VERSION=$MAJOR.$MINOR.$PATCH
+
+sed -i -e "s/version: $CURRENT_VERSION+$CURRENT_BUILD_NUMBER/version: $NEXT_VERSION+$NEXT_BUILD_NUMBER/" pubspec.yaml
+
+echo "APP_VERSION=v$NEXT_VERSION" >> "$GITHUB_ENV"
