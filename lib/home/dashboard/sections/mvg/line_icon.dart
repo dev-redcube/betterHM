7ad326f41c1c66e004a77e6e64ede3e4a35a97f4
Moritz Huber <2nd@moritzhuber.de>
2024-06-23 00:23:23 +0200

    add more stations and canteens (#175)

--- lib/home/dashboard/sections/mvg/line_icon.dart
+++ lib/home/dashboard/sections/mvg/line_icon.dart
@@ -16,11 +16,17 @@ final lineColors = <String, (Color, BoxDecoration)>{
   ), // Tram
   "N20": (Colors.white, const BoxDecoration(color: Color(0xFF16bae7))), // Tram
   "153": genericBus, // Bus
+  "154": genericBus, // Bus
   // Avenariusplatz
   "160": genericBus, // Bus
+  "230": genericBus, // Bus
+  "292": genericBus, // Bus
+  "690": genericBus, // Bus
   // Planegger Straße
   "56": (Colors.white, const BoxDecoration(color: Color(0xFFec6726))), // Bus
   "57": (Colors.white, const BoxDecoration(color: Color(0xFFec6726))), // Bus
+  "58": (Colors.white, const BoxDecoration(color: Color(0xFFec6726))), // Bus
+  "68": (Colors.white, const BoxDecoration(color: Color(0xFFec6726))), // Bus
   "259": genericBus, // Bus
   "265": genericBus, // Bus
   // Ottostraße
@@ -40,6 +46,12 @@ final lineColors = <String, (Color, BoxDecoration)>{
     const Color(0xFFf2ad13),
     const BoxDecoration(color: Colors.black)
   ), // Tram
+  "X201": (Colors.white, const BoxDecoration(color: Color(0xFF244a9a))),
+  "X660": (Colors.white, const BoxDecoration(color: Color(0xFF244a9a))),
+  "U1": (Colors.white, const BoxDecoration(color: Color(0xFF438136))),
+  "U2": (Colors.white, const BoxDecoration(color: Color(0xFFC40C37))),
+  "U3": (Colors.white, const BoxDecoration(color: Color(0xFFF36E31))),
+  "U6": (Colors.white, const BoxDecoration(color: Color(0xFF006CB3))),
 };
 
 class LineIcon extends StatelessWidget {
