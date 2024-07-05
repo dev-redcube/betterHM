d84c0ce5894b94f0badf906c4bdcabbc490fc4e8
Moritz Huber <2nd@moritzhuber.de>
2024-07-05 19:30:11 +0200

    kalender items besser unterscheidbar (#178)

---
+++ lib/home/calendar/components/multi_day_tile.dart
@@ -0,0 +1,70 @@
+import 'package:animations/animations.dart';
+import 'package:better_hm/home/calendar/calendar_service.dart';
+import 'package:better_hm/home/calendar/detail_screen.dart';
+import 'package:better_hm/shared/extensions/extensions_context.dart';
+import 'package:flutter/material.dart';
+import 'package:kalender/kalender.dart';
+
+class MultiDayTile extends StatelessWidget {
+  const MultiDayTile({
+    super.key,
+    required this.event,
+    required this.configuration,
+    required this.colors,
+  });
+
+  final CustomCalendarEvent event;
+  final MultiDayTileConfiguration configuration;
+
+  final (Color, Color) colors;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 2, right: 4),
+      child: OpenContainer(
+        closedShape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(8),
+        ),
+        closedColor: colors.$1,
+        openColor: context.theme.colorScheme.surface,
+        closedElevation: 0,
+        useRootNavigator: true,
+        openBuilder: (context, action) {
+          return CalendarDetailScreen(event: event);
+        },
+        closedBuilder: (context, action) {
+          return Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+              side: BorderSide(color: colors.$1),
+            ),
+            margin: EdgeInsets.zero,
+            elevation: configuration.tileType == TileType.selected ? 8 : 0,
+            color: configuration.tileType == TileType.ghost
+                ? colors.$1.withAlpha(100)
+                : colors.$1,
+            child: Padding(
+              padding: const EdgeInsets.only(left: 4),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: configuration.tileType != TileType.ghost
+                    ? Text(
+                        event.eventData?.component.summary?.value.value ??
+                            "No title",
+                        // style: TextStyle(color: colors.$2),
+                        style: context.theme.textTheme.bodySmall?.copyWith(
+                          color: colors.$2,
+                        ),
+                        overflow: TextOverflow.clip,
+                        softWrap: false,
+                      )
+                    : null,
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
