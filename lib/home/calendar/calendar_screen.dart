4c39d31729be8e5ac10d5f54a8a8fbe31109d43f
Moritz Huber <2nd@moritzhuber.de>
2025-04-22 17:19:57 +0200

    remove old stuff

--- lib/home/calendar/calendar_screen.dart
+++ lib/home/calendar/calendar_screen.dart
@@ -1,12 +1,6 @@
-import 'package:better_hm/home/calendar/calendar_body.dart';
-import 'package:better_hm/home/calendar/edit_screen.dart';
-import 'package:better_hm/home/calendar/ical_sync_state.dart';
 import 'package:better_hm/i18n/strings.g.dart';
 import 'package:better_hm/shared/extensions/extensions_context.dart';
-import 'package:better_hm/shared/prefs.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 
 class CalendarScreen extends StatelessWidget {
   const CalendarScreen({super.key});
@@ -20,86 +14,8 @@ class CalendarScreen extends StatelessWidget {
           100,
         ),
         scrolledUnderElevation: 0.0,
-        actions: [
-          IconButton(
-            onPressed: () {
-              Prefs.calendarViewConfiguration.value =
-                  (Prefs.calendarViewConfiguration.value + 1) %
-                  calendarViewConfigurations.length;
-            },
-            icon: const _CalendarViewConfigurationIcon(),
-          ),
-          IconButton(
-            icon: const Icon(Icons.today_rounded),
-            onPressed: () {
-              calendarController.animateToDate(DateTime.now());
-            },
-            tooltip: t.calendar.today,
-          ),
-          IconButton(
-            icon: const Icon(Icons.edit_calendar_rounded),
-            onPressed: () {
-              context.pushNamed(CalendarEditScreen.routeName);
-            },
-            tooltip: t.calendar.edit.tooltip,
-          ),
-        ],
       ),
-      body: const Stack(
-        alignment: Alignment.topCenter,
-        children: [CalendarBody(), SyncProgress()],
-      ),
-    );
-  }
-}
-
-class _CalendarViewConfigurationIcon extends StatefulWidget {
-  const _CalendarViewConfigurationIcon();
-
-  @override
-  State<_CalendarViewConfigurationIcon> createState() =>
-      _CalendarViewConfigurationIconState();
-}
-
-class _CalendarViewConfigurationIconState
-    extends State<_CalendarViewConfigurationIcon> {
-  @override
-  initState() {
-    super.initState();
-    Prefs.calendarViewConfiguration.addListener(_rebuild);
-  }
-
-  _rebuild() {
-    setState(() {});
-  }
-
-  @override
-  void dispose() {
-    Prefs.calendarViewConfiguration.removeListener(_rebuild);
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) => switch (Prefs
-      .calendarViewConfiguration
-      .value) {
-    0 => const Icon(Icons.calendar_view_day_rounded),
-    1 => const Icon(Icons.calendar_view_week_rounded),
-    2 => const Icon(Icons.calendar_view_month_rounded),
-    // Never happening
-    _ => const Icon(Icons.error_rounded),
-  };
-}
-
-class SyncProgress extends ConsumerWidget {
-  const SyncProgress({super.key});
-
-  @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    final value = ref.watch(iCalSyncStateNotifierProvider).value;
-    return SizedBox(
-      height: value?.syncProgress == ICalSyncProgressEnum.inProgress ? null : 0,
-      child: LinearProgressIndicator(value: value?.progressInPercent),
+      body: SizedBox.shrink(),
     );
   }
 }
